6e5986dc532d67da298d8761f780aaaeade91e03
Galen.Guo <galen.guo@medi-plus.com.cn>
2019-06-02 23:15:36 +0800

    rxjs

---
+++ src/views/Rxjs/demo1.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Observable } from 'rxjs/Observable';
+
+// const oneObservable = Observable.create(subscriber => {
+//   subscriber.next('🍎')
+//   subscriber.next('🍊')
+//   subscriber.complete()
+// })
+
+// const oneObserver = {
+//   next: data => console.log(data),
+//   complete: () => console.log('done!')
+// }
+
+// oneObservable.subscribe(oneObserver)
+
+var myObservable = Observable.create(observer => {
+  observer.next('foo');
+  setTimeout(() => observer.next('bar'), 1000);
+});
+myObservable.subscribe(value => console.log(value));
+
+</script>
